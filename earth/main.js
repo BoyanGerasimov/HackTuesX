6638a96bb0e32c35093f020517fbd2b4b260ec17
BoyanGerasimov <bobkata12753@gmail.com>
2024-03-15 10:31:35 +0200

    earth spinning + stars

--- earth/main.js
+++ earth/main.js
@@ -1,29 +1,60 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+  dragging: false
+};
+
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+
+});
+
+const starVertices = [];
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000;
+  const y = (Math.random() - 0.5) * 2000;
+  const z = -Math.random() * 2000;
+  starVertices.push(x, y, z);
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+
+scene.add(stars);
+
 const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
-renderer.setSize(innerWidth, innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const sphere = new THREE.Mesh
-(
-new THREE.SphereGeometry(7, 50, 50), 
-new THREE.MeshBasicMaterial(
-  {
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(7, 50, 50),
+  new THREE.MeshBasicMaterial({
     map: new THREE.TextureLoader().load('./img/earth.jpeg')
-  }));
-
+  })
+);
 scene.add(sphere);
+
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group);
 
 camera.position.z = 25;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  sphere.rotation.y += 0.005;
+  sphere.rotation.x += 0.0001;
 }
 animate();
+
